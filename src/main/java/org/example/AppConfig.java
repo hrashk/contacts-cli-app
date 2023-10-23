09b182d3f7384b74d0f3b6a7dfb1ab5740dbde55
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-10-23 21:37:42 +0200

    use annotation instead of a static config bean

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -3,26 +3,18 @@ package org.example;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.PropertySource;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 @Configuration
 @ComponentScan("org.example")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
     @Bean
     public Reader systemInputReader() {
         return new InputStreamReader(System.in);
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        var props = new PropertySourcesPlaceholderConfigurer();
-        props.setLocation(new ClassPathResource("application.properties"));
-
-        return props;
     }
 }
